ac66e86b340bb3faf244a316d2b2321c88f3c7bf
Jozafath Perez <jozperez@estudiantec.cr>
2024-05-25 17:05:52 -0600

    Inicio de programa

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { Home } from "./pages/Home";
+import { Contacts } from "./pages/Contacts";
+import { Account } from "./pages/Account";
+import { Catalog } from "./pages/Catalog";
+import { Cart } from "./pages/Cart";
+
+
+
+const Menu = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Menu.Navigator>
+        <Menu.Screen
+          name="Home"
+          options={{
+            headerTitle: "Inicio",
+          }}
+          component={Home}
+        />
+        <Menu.Screen name="Contacts" component={Contacts} />
+        <Menu.Screen name="Account" component={Account} />
+        <Menu.Screen name="Catalog" component={Catalog} />
+        <Menu.Screen name="Cart" component={Cart} />
+      </Menu.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
