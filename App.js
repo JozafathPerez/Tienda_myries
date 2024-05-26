9bbdb695a9fa9cf52a7e206f2d782c8b34e55ee6
Jozafath Perez <jozperez@estudiantec.cr>
2024-05-26 01:54:59 -0600

    Estilo de la barra de navegación

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import "react-native-gesture-handler";
+
+// Imports de las paginas
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
 import Account from "./pages/Account";
 import Catalog from "./pages/Catalog";
 import Cart from "./pages/Cart";
 
+// Import del estilo de la barra de navegador
+import { CartScreenOptions, homeScreenOptions, otherScreenOptions } from './NavigatorStyle';
+
 const Menu = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Menu.Navigator>
-        <Menu.Screen name="Inicio" component={Home} options={{ headerTitle: "Inicio" }} />
-        <Menu.Screen name="Catalogo" component={Catalog} />
-        <Menu.Screen name="Carrito" component={Cart} />
-        <Menu.Screen name="Cuenta" component={Account} />
-        <Menu.Screen name="Contactenos" component={Contacts} />
+      <Menu.Navigator screenOptions={{ drawerPosition: 'right' }}>
+        <Menu.Screen name="Inicio" component={Home} options={homeScreenOptions} />
+        <Menu.Screen name="Catalogo" component={Catalog} options={homeScreenOptions} />
+        <Menu.Screen name="Carrito" component={Cart} options={CartScreenOptions} />
+        <Menu.Screen name="Cuenta" component={Account} options={otherScreenOptions} />
+        <Menu.Screen name="Contactenos" component={Contacts} options={otherScreenOptions} />
       </Menu.Navigator>
     </NavigationContainer>
   );
---
+++ NavigatorStyle.jsx
@@ -0,0 +1,93 @@
+import { Ionicons } from "@expo/vector-icons"; 
+import { View, Image, StyleSheet } from "react-native";
+
+const Logo = () => (
+    <Image
+      source={require('./Objects/Images/Logo(Myries).png')}
+      style={{ width: 90, height: 50, marginLeft: 10 }}
+    />
+  );
+  
+  const IconContainer = ({ children }) => (
+    <View style={styles.iconContainer}>
+      {children}
+    </View>
+  );
+  
+  const commonHeaderOptions = ({ navigation }) => ({
+    headerStyle: {
+      height: 100,
+    },
+    headerTitleStyle: {
+      fontSize: 24,
+    },
+    headerLeft: () => <Logo />,
+    headerRight: () => (
+      <IconContainer>
+        <Ionicons 
+          name="menu-outline" 
+          size={30} 
+          color="black" 
+          onPress={() => navigation.openDrawer()}
+        />
+      </IconContainer>
+    ),
+    drawerIcon: () => false,
+  });
+  
+  const homeScreenOptions = ({ navigation }) => {
+    const options = commonHeaderOptions({ navigation });
+    return {
+      ...options,
+      headerRight: () => (
+        <IconContainer>
+          <Ionicons 
+            name="search-outline" 
+            size={30} 
+            color="black" 
+            onPress={() => navigation.navigate('Buscar')} 
+            style={{ marginRight: 20 }}
+          />
+          <Ionicons 
+            name="cart-outline" 
+            size={30} 
+            color="black" 
+            onPress={() => navigation.navigate('Carrito')} 
+            style={{ marginRight: 20 }}
+          />
+          {options.headerRight()}
+        </IconContainer>
+      ),
+    };
+  };
+  
+  const otherScreenOptions = ({ navigation }) => {
+    const options = commonHeaderOptions({ navigation });
+    return {
+      ...options,
+      headerRight: () => (
+        <IconContainer>
+          <Ionicons 
+            name="cart-outline" 
+            size={30} 
+            color="black" 
+            onPress={() => navigation.navigate('Carrito')} 
+            style={{ marginRight: 20 }}
+          />
+          {options.headerRight()}
+        </IconContainer>
+      ),
+    };
+  };
+  
+  const CartScreenOptions = commonHeaderOptions;
+
+  const styles = StyleSheet.create({
+    iconContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingRight: 10,
+    },
+  });
+
+  export { CartScreenOptions, homeScreenOptions, otherScreenOptions };
