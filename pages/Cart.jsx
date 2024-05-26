f783cd14b4abac4f9ea048e66e07df98e667868c
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-05-25 18:14:40 -0600

    semi implementacion de tailwindcss

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -1,18 +1,9 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View } from "react-native";
 
 export function Cart() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Carrito</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
