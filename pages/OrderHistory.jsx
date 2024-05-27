a1b7c082db170ffe376f28b61f7ba35324eb9b56
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-05-26 22:44:58 -0600

    little style to the little history

--- pages/OrderHistory.jsx
+++ pages/OrderHistory.jsx
@@ -10,16 +10,16 @@ export default function OrderHistory() {
   const { orders } = useContext(OrderContext);
 
   const renderOrderItem = ({ item }) => (
-    <StyledView className="p-5 bg-white rounded-lg shadow-lg mb-4 border border-gray-300">
-      <StyledView className="flex-row justify-between items-center mb-3">
-        <StyledText className="text-xl font-bold text-blue-700">Pedido #{item.id}</StyledText>
-        <StyledText className="text-sm text-gray-500">{item.date}</StyledText>
+    <StyledView className="pb-3 bg-white rounded-lg shadow-lg mb-4 border-gray-300">
+      <StyledView className="px-4 py-1 flex-row justify-between items-center mb-3 bg-slate-300 rounded-t-lg border-b border-blue-500">
+        <StyledText className="text-xl font-extrabold text text-black">Pedido #{item.id}</StyledText>
+        <StyledText className="text-sm text-gray-600">{item.date}</StyledText>
       </StyledView>
-      <StyledText className="text-base font-semibold mb-2">Estado: <StyledText className="text-blue-500">{item.status}</StyledText></StyledText>
-      <StyledText className="text-base font-semibold mb-2">Productos:</StyledText>
+      <StyledText className="text-base font-semibold mb-2 mx-4">Estado: <StyledText className="text-blue-500">{item.status}</StyledText></StyledText>
+      <StyledText className="text-base font-semibold mb-2 mx-4">Productos:</StyledText>
       {item.items.map((product, index) => (
-        <StyledView key={index} className="ml-4 mb-1">
-          <StyledText className="text-base">- {product.title} <StyledText className="font-semibold">x {product.quantity}</StyledText></StyledText>
+        <StyledView key={index} className="ml-8 mb-1">
+          <StyledText className="text-base">- {product.title} <StyledText className="font-bold">x {product.quantity}</StyledText></StyledText>
         </StyledView>
       ))}
     </StyledView>
@@ -27,7 +27,7 @@ export default function OrderHistory() {
 
   return (
     <StyledView className="flex-1 p-5 bg-gray-100">
-      <StyledText className="text-3xl font-bold mb-5 text-center text-blue-700">Historial de Pedidos</StyledText>
+      {/* <StyledText className="text-3xl font-bold mb-5 text-center text-blue-500">Historial de Pedidos</StyledText> */}
       <FlatList
         data={orders}
         keyExtractor={(item) => item.id.toString()}
